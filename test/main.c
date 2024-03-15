e671017747e99b2bd2241fcacc10ac81fb84d508
Anotra <Anotra9999@gmail.com>
2024-03-15 19:46:32 -0400

    clean up tests

--- test/main.c
+++ test/main.c
@@ -3,25 +3,29 @@
 #include <stdio.h>
 #include <string.h>
 
-#define TEST_FUNC(name) \
-  int test_ ## name(void)
-
-
-TEST_FUNC(locks);
-TEST_FUNC(ordering);
-
-
-#define TEST_FEATURE(name)\
-  do {\
-    if (0 == strcmp(#name, argv[1])) {\
-      return test_ ## name();\
-    }\
+#define TEST_FEATURE(name)                     \
+  do {                                         \
+    int test_ ## name(void);                   \
+    if (0 == strcmp(#name, argv[1]))           \
+      return test_ ## name();                  \
   } while (0)
+
+static int _run_test(int argc, char *argv[]);
 
 int main(int argc, char *argv[]) {
   if (argc != 2)
     return 1;
+  return _run_test(argc, argv);
+}
+
+int
+_run_test(int argc, char *argv[]) {
+
+#ifdef WITH_LOCKS
   TEST_FEATURE(locks);
+#endif
+
   TEST_FEATURE(ordering);
+
   return -1;
 }
